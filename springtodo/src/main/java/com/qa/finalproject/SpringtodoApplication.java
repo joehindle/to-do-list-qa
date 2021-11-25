fee6d169680fc17c7bcdd66c0de5139c90c10e70
joehindle <HindleJoe2002@gmail.com>
2021-11-25 05:57:33 +0000

    made a simple spring project

---
+++ springtodo/src/main/java/com/qa/finalproject/SpringtodoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtodoApplication.class, args);
+	}
+
+}
---
+++ springtodo/src/test/java/com/qa/finalproject/SpringtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
